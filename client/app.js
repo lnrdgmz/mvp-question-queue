23b6e17888ae3bea1337733eac6ad351d8679a85
Leo Gomez <leogomez@gmail.com>
2017-04-24 23:38:09 -0400

    Retrieve questions at a 5-second interval

--- client/app.js
+++ client/app.js
@@ -1,12 +1,16 @@
 angular.module('app', [])
 
-  .controller('QuestionViewController', function ($http, $scope) {
+  .controller('QuestionViewController', function ($http, $scope, $interval) {
     $scope.questions = [];
-    $http.get('http://localhost:3000/questions')
-      .then(function (res) {
-        console.log(res.data);
-        $scope.questions = res.data;
-      });
+    $scope.getQuestions = function () {
+      $http.get('http://localhost:3000/questions')
+        .then(function (res) {
+          console.log(res.data);
+          $scope.questions = res.data;
+        });
+    };
+    $scope.getQuestions();
+    $interval($scope.getQuestions, 5000);
   })
   .controller('FormController', function ($scope) {
     $scope.myAlert = function (str) {
