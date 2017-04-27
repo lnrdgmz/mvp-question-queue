c748905a75affd7dbb86801450b176be4a3788e3
Leo Gomez <leogomez@gmail.com>
2017-04-27 11:06:44 -0400

    Point to credentials.json

--- app.js
+++ app.js
@@ -11,18 +11,19 @@ const handlers = require('./handlers.js');
 // const db = require('./db.js');
 
 const google = require('googleapis');
-const credentials = {
-  "type": process.env.GOOGLECRED_TYPE,
-  "project_id": process.env.GOOGLECRED_PROJECT_ID,
-  "private_key_id": process.env.GOOGLECRED_PRIVATE_KEY_ID,
-  "private_key": process.env.GOOGLECRED_PRIVATE_KEY,
-  "client_email": process.env.GOOGLECRED_CLIENT_EMAIL,
-  "client_id": process.env.GOOGLECRED_CLIENT_ID,
-  "auth_uri": process.env.GOOGLECRED_AUTH_URI,
-  "token_uri": process.env.GOOGLECRED_TOKEN_URI,
-  "auth_provider_x509_cert_url": process.env.GOOGLECRED_AUTH_PROVIDER_X509_CERT_URL,
-  "client_x509_cert_url":  process.env.GOOGLECRED_CLIENT_X509_CERT_URL
-};
+const credentials = require('./config/credentials.json')
+      // {
+//   "type": process.env.GOOGLECRED_TYPE,
+//   "project_id": process.env.GOOGLECRED_PROJECT_ID,
+//   "private_key_id": process.env.GOOGLECRED_PRIVATE_KEY_ID,
+//   "private_key": process.env.GOOGLECRED_PRIVATE_KEY,
+//   "client_email": process.env.GOOGLECRED_CLIENT_EMAIL,
+//   "client_id": process.env.GOOGLECRED_CLIENT_ID,
+//   "auth_uri": process.env.GOOGLECRED_AUTH_URI,
+//   "token_uri": process.env.GOOGLECRED_TOKEN_URI,
+//   "auth_provider_x509_cert_url": process.env.GOOGLECRED_AUTH_PROVIDER_X509_CERT_URL,
+//   "client_x509_cert_url":  process.env.GOOGLECRED_CLIENT_X509_CERT_URL
+// };
 
 const auth = new google.auth.JWT(
   credentials.client_email,
